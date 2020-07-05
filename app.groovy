0c13fb7e12cc8f3e63eed854e7c744cfd1cc0046
Minato Kasai <bcmk1220158@gn.iwasaki.ac.jp>
2020-07-05 20:54:47 +0900

    use thymeleaf

--- app.groovy
+++ app.groovy
@@ -1,21 +1,12 @@
-@RestController
-class HelloApplication {
+@Grab("thymeleaf-spring5")
+
+@Controller
+class App {
+
     @RequestMapping("/")
-    def hello() {
-        "Hello, world!"
-    }
-
-    @RequestMapping("/bye")
-    def bye() {
-        "Good Bye, world!"
-    }
-
-    @RequestMapping("/home")
-    def home() {
-        def header = "<html><body>"
-        def footer = "</body></html>"
-        def content = "<h1>Hello!</h1><p>this is html content.</p>"
-
-        header + content + footer
+    @ResponseBody
+    def home(ModelAndView mav) {
+        mav.setViewName("home")
+        mav
     }
 }
