e6ab3b52e6f42ff0289b413cc7b32678be435ba5
Minato Kasai <bcmk1220158@gn.iwasaki.ac.jp>
2020-07-05 21:56:19 +0900

    [update]thymeleaf form send method

--- app.groovy
+++ app.groovy
@@ -7,7 +7,16 @@ class App {
     @ResponseBody
     def home(ModelAndView mav) {
         mav.setViewName("home")
-        mav.addObject("msg", "this is msg.")
+        mav.addObject("msg", "名前を入力して送信してください")
+        mav
+    }
+
+    @RequestMapping(value="/send", method=RequestMethod.POST)
+    @ResponseBody
+    def send(@RequestParam("text1")String str, ModelAndView mav) {
+        mav.setViewName("home")
+        mav.addObject("msg", "こんにちは、" + str + "!!")
+        mav.addObject("value", str)
         mav
     }
 }
