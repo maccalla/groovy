58f16d2eafa135b70675cd84631e9d0ce074037b
Minato Kasai <bcmk1220158@gn.iwasaki.ac.jp>
2020-07-05 20:31:19 +0900

    [update]use html

--- app.groovy
+++ app.groovy
@@ -9,4 +9,13 @@ class HelloApplication {
     def bye() {
         "Good Bye, world!"
     }
+
+    @RequestMapping("/home")
+    def home() {
+        def header = "<html><body>"
+        def footer = "</body></html>"
+        def content = "<h1>Hello!</h1><p>this is html content.</p>"
+
+        header + content + footer
+    }
 }
